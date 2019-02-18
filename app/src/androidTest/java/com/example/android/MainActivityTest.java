ea52ce67ddb44f0e51cf41a8f9331afd4049fecc
Yenimi <pavan.yenimi@nineleaps.com>
2019-02-18 18:15:06 +0530

    unit test for splash screen  activity and next activity working

--- app/src/androidTest/java/com/example/android/MainActivityTest.java
+++ app/src/androidTest/java/com/example/android/MainActivityTest.java
@@ -5,34 +5,42 @@ import android.app.Instrumentation;
 import android.support.test.rule.ActivityTestRule;
 import android.support.v7.app.AppCompatActivity;
 
+import junit.framework.Assert;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
 
 public class MainActivityTest {
     @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule=new ActivityTestRule<MainActivity>(MainActivity.class);
+    public ActivityTestRule<MainActivity> mActivityTestRule=
+            new ActivityTestRule<MainActivity>(MainActivity.class);
     private MainActivity mActivity=null;
-    Instrumentation.ActivityMonitor activityMonitor= getInstrumentation().addMonitor(next.class.getName(),null,false);
+    private MainActivity mA=null;
+    Instrumentation.ActivityMonitor activityMonitor= getInstrumentation().
+            addMonitor(next.class.getName(),null,false);
 
 
     @Before
     public void setUp() throws Exception {
         mActivity=mActivityTestRule.getActivity();
+        mA=mActivityTestRule.getActivity();
 
     }
     @Test
     public  void Test()
     {
-       Activity nextactivity= getInstrumentation().waitForMonitorWithTimeout(activityMonitor,4000);
+        assertNotNull(mActivity.findViewById(R.id.tvmain));
+        assertNotNull(mActivity);
+        Activity nextactivity= getInstrumentation().waitForMonitorWithTimeout
+                (activityMonitor,2000);
        assertNotNull(nextactivity);
-    }
-
-
+     }
 
     @After
     public void tearDown() throws Exception {
